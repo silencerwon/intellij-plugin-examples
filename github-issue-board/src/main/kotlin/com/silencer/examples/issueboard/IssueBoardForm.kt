787d8e41f3c472f0c2b1de0b116b5ca7c2fd47e2
silencer <silencerwon@gmail.com>
2019-09-05 01:22:17 +0900

    github issue api 호출용 client
    - ui form 파일을 kotlin 버전으로 변경

--- github-issue-board/src/main/kotlin/com/silencer/examples/issueboard/IssueBoardForm.kt
+++ github-issue-board/src/main/kotlin/com/silencer/examples/issueboard/IssueBoardForm.kt
@@ -1,6 +1,43 @@
-import javax.swing.*;
+import com.intellij.openapi.components.ServiceManager
+import com.silencer.examples.issueboard.Issue
+import com.silencer.examples.issueboard.IssueClient
+import java.awt.event.ActionListener
+import javax.swing.*
+import javax.swing.table.DefaultTableColumnModel
+import javax.swing.table.DefaultTableModel
+import javax.swing.table.JTableHeader
 
-public class IssueBoardForm {
-	private JPanel panel1;
-	private JTable table1;
+class IssueBoardForm {
+    var contentPanel: JPanel? = null
+    var issueTable: JTable? = null
+    var addButton: JButton? = null
+    var refreshButton: JButton? = null
+
+    constructor() {
+        refreshButton!!.addActionListener(ActionListener {
+            val issueClient = ServiceManager.getService(IssueClient::class.java)
+
+            val issueList = issueClient.getIssueList();
+
+            refreshIssueTable(issueList)
+        })
+    }
+
+    private fun refreshIssueTable(issueList: List<Issue>) {
+        val columnModel = DefaultTableColumnModel()
+        val tableModel = DefaultTableModel()
+        val tableHeader = JTableHeader(columnModel)
+
+        tableModel.addColumn("issueNo")
+        tableModel.addColumn("title")
+        tableModel.addColumn("state")
+        tableModel.addColumn("url")
+
+        issueList.forEach {
+            tableModel.addRow(it.asArray())
+        }
+
+        issueTable!!.tableHeader = tableHeader
+        issueTable!!.model = tableModel
+    }
 }
